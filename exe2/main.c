7b344f2173555e56844409a849a5e6839491d9b3
victor <victorfs2@al.insper.edu.br>
2025-03-30 14:23:03 -0300

    lab1 feito novamente 3.0

--- exe2/main.c
+++ exe2/main.c
@@ -1,71 +1,71 @@
-#include "hardware/gpio.h"
-#include "pico/stdlib.h"
-#include <stdio.h>
-
-const int BTN_AZUL = 7;
-const int BTN_VERDE = 26;
-
-int main() {
-  stdio_init_all();
-
-  gpio_init(BTN_AZUL);
-  gpio_init(BTN_VERDE);
-
-  gpio_set_dir(BTN_AZUL, GPIO_IN );
-  gpio_set_dir(BTN_VERDE, GPIO_IN );
-
-  gpio_pull_up(BTN_AZUL);
-  gpio_pull_up(BTN_VERDE);
-
-  while (true) {
-    if (!gpio_get(BTN_AZUL)){
-      printf("Botao 1 \n");
-      while(!gpio_get(BTN_AZUL)){
-
-      }
-
-    }
-    if (!gpio_get(BTN_VERDE)){
-      printf("Botao 2 \n");
-      while(!gpio_get(BTN_VERDE)){
-        
-      }
-    }
-  }
-}
-
-
-
-
 // #include "hardware/gpio.h"
 // #include "pico/stdlib.h"
 // #include <stdio.h>
 
-// const int BTN_VERDE = 26;
 // const int BTN_AZUL = 7;
+// const int BTN_VERDE = 26;
 
 // int main() {
 //   stdio_init_all();
 
-//   gpio_init(BTN_VERDE);
 //   gpio_init(BTN_AZUL);
+//   gpio_init(BTN_VERDE);
 
-//   gpio_set_dir(BTN_VERDE, GPIO_IN);
-//   gpio_set_dir(BTN_AZUL, GPIO_IN);
+//   gpio_set_dir(BTN_AZUL, GPIO_IN );
+//   gpio_set_dir(BTN_VERDE, GPIO_IN );
 
-//   gpio_pull_up(BTN_VERDE);
 //   gpio_pull_up(BTN_AZUL);
+//   gpio_pull_up(BTN_VERDE);
 
 //   while (true) {
-//     if (!gpio_get(BTN_VERDE)) {
-//       printf("Botao 1\n");
-//       while (!gpio_get(BTN_VERDE)) {
-//       };
-//     }
 //     if (!gpio_get(BTN_AZUL)){
-//       printf("Botao 2\n");
-//       while (!gpio_get(BTN_AZUL)) {
-//       };
+//       printf("Botao 1 \n");
+//       while(!gpio_get(BTN_AZUL)){
+
+//       }
+
+//     }
+//     if (!gpio_get(BTN_VERDE)){
+//       printf("Botao 2 \n");
+//       while(!gpio_get(BTN_VERDE)){
+        
+//       }
 //     }
 //   }
 // }
+
+
+
+
+#include "hardware/gpio.h"
+#include "pico/stdlib.h"
+#include <stdio.h>
+
+const int BTN_VERDE = 26;
+const int BTN_AZUL = 7;
+
+int main() {
+  stdio_init_all();
+
+  gpio_init(BTN_VERDE);
+  gpio_init(BTN_AZUL);
+
+  gpio_set_dir(BTN_VERDE, GPIO_IN);
+  gpio_set_dir(BTN_AZUL, GPIO_IN);
+
+  gpio_pull_up(BTN_VERDE);
+  gpio_pull_up(BTN_AZUL);
+
+  while (true) {
+    if (!gpio_get(BTN_VERDE)) {
+      printf("Botao 1\n");
+      while (!gpio_get(BTN_VERDE)) {
+      };
+    }
+    if (!gpio_get(BTN_AZUL)){
+      printf("Botao 2\n");
+      while (!gpio_get(BTN_AZUL)) {
+      };
+    }
+  }
+}
--- exe3/main.c
+++ exe3/main.c
@@ -1,67 +1,3 @@
-// #include "hardware/gpio.h"
-// #include "pico/stdlib.h"
-// #include <stdio.h>
-
-// const int BTN_GREEN = 26;
-// const int BTN_RED = 28;
-// const int LED_GREEN = 6;
-// const int LED_RED = 4;
-
-// int main() {
-//   stdio_init_all();
-
-//   gpio_init(BTN_GREEN);
-//   gpio_init(BTN_RED);
-//   gpio_init(LED_GREEN);
-//   gpio_init(LED_RED);
-
-//   gpio_set_dir(BTN_GREEN, GPIO_IN);
-//   gpio_set_dir(BTN_RED, GPIO_IN);
-//   gpio_set_dir(LED_GREEN, GPIO_OUT);
-//   gpio_set_dir(LED_RED, GPIO_OUT);
-
-//   gpio_pull_up(BTN_GREEN);
-//   gpio_pull_up(BTN_RED);
-
-//   int estado_atual_led = 0;
-//   int estado_atual_botao;
-//   int ultimo_estado_botao = 1;
-
-//   int estado_atual_led_red = 0;
-//   int estado_atual_botao_red;
-//   int ultimo_estado_botao_red = 1;
-
-//   while (true) {
-//     estado_atual_botao = gpio_get(BTN_GREEN);
-//     if (ultimo_estado_botao == 1 && estado_atual_botao == 0) {
-//       if (estado_atual_led == 0) {
-//         estado_atual_led = 1;
-//       } else {
-//           estado_atual_led = 0;
-//       }
-//       gpio_put(LED_GREEN, estado_atual_led);
-//       sleep_ms(50);
-//     }
-//     ultimo_estado_botao = estado_atual_botao;
-
-//     estado_atual_botao_red = gpio_get(BTN_RED);
-//     if (ultimo_estado_botao_red == 1 && estado_atual_botao_red == 0) {
-//       if (estado_atual_led_red == 0) {
-//         estado_atual_led_red = 1;
-//       } else {
-//           estado_atual_led_red = 0;
-//       }
-//       gpio_put(LED_RED, estado_atual_led_red);
-//       sleep_ms(50);
-//     }
-//     ultimo_estado_botao_red = estado_atual_botao_red;
-//   }
-// }
-
-
-
-
-
 #include "hardware/gpio.h"
 #include "pico/stdlib.h"
 #include <stdio.h>
@@ -74,40 +10,104 @@ const int LED_RED = 4;
 int main() {
   stdio_init_all();
 
-  gpio_init(BTN_RED);
   gpio_init(BTN_GREEN);
+  gpio_init(BTN_RED);
   gpio_init(LED_GREEN);
   gpio_init(LED_RED);
 
-  gpio_set_dir(BTN_RED, GPIO_IN );
-  gpio_set_dir(BTN_GREEN, GPIO_IN );
-  gpio_set_dir(LED_GREEN, GPIO_OUT );
-  gpio_set_dir(LED_RED, GPIO_OUT );
+  gpio_set_dir(BTN_GREEN, GPIO_IN);
+  gpio_set_dir(BTN_RED, GPIO_IN);
+  gpio_set_dir(LED_GREEN, GPIO_OUT);
+  gpio_set_dir(LED_RED, GPIO_OUT);
 
-  gpio_pull_up(BTN_RED);
   gpio_pull_up(BTN_GREEN);
+  gpio_pull_up(BTN_RED);
+
+  int estado_atual_led = 0;
+  int estado_atual_botao;
+  int ultimo_estado_botao = 1;
+
+  int estado_atual_led_red = 0;
+  int estado_atual_botao_red;
+  int ultimo_estado_botao_red = 1;
+
+  while (true) {
+    estado_atual_botao = gpio_get(BTN_GREEN);
+    if (ultimo_estado_botao == 1 && estado_atual_botao == 0) {
+      if (estado_atual_led == 0) {
+        estado_atual_led = 1;
+      } else {
+          estado_atual_led = 0;
+      }
+      gpio_put(LED_GREEN, estado_atual_led);
+      sleep_ms(50);
+    }
+    ultimo_estado_botao = estado_atual_botao;
+
+    estado_atual_botao_red = gpio_get(BTN_RED);
+    if (ultimo_estado_botao_red == 1 && estado_atual_botao_red == 0) {
+      if (estado_atual_led_red == 0) {
+        estado_atual_led_red = 1;
+      } else {
+          estado_atual_led_red = 0;
+      }
+      gpio_put(LED_RED, estado_atual_led_red);
+      sleep_ms(50);
+    }
+    ultimo_estado_botao_red = estado_atual_botao_red;
+  }
+}
+
+
+
+
+
+// #include "hardware/gpio.h"
+// #include "pico/stdlib.h"
+// #include <stdio.h>
+
+// const int BTN_GREEN = 26;
+// const int BTN_RED = 28;
+// const int LED_GREEN = 6;
+// const int LED_RED = 4;
+
+// int main() {
+//   stdio_init_all();
+
+//   gpio_init(BTN_RED);
+//   gpio_init(BTN_GREEN);
+//   gpio_init(LED_GREEN);
+//   gpio_init(LED_RED);
+
+//   gpio_set_dir(BTN_RED, GPIO_IN );
+//   gpio_set_dir(BTN_GREEN, GPIO_IN );
+//   gpio_set_dir(LED_GREEN, GPIO_OUT );
+//   gpio_set_dir(LED_RED, GPIO_OUT );
+
+//   gpio_pull_up(BTN_RED);
+//   gpio_pull_up(BTN_GREEN);
 
  
 
 
-  while (true) {
-    int status_red = 0;
-    int status_green = 0;
+//   while (true) {
+//     int status_red = 0;
+//     int status_green = 0;
 
-    if (!gpio_get(BTN_RED)){
-      gpio_put(LED_RED, !status_red);
-      while(!gpio_get(BTN_RED)){
+//     if (!gpio_get(BTN_RED)){
+//       gpio_put(LED_RED, !status_red);
+//       while(!gpio_get(BTN_RED)){
 
-      }
+//       }
 
-    }
-    if (!gpio_get(BTN_GREEN)){
-      gpio_put(LED_GREEN, !status_green);
-      while(!gpio_get(BTN_GREEN)){
+//     }
+//     if (!gpio_get(BTN_GREEN)){
+//       gpio_put(LED_GREEN, !status_green);
+//       while(!gpio_get(BTN_GREEN)){
         
-      }
-    }
+//       }
+//     }
    
-  }
-}
+//   }
+// }
 
