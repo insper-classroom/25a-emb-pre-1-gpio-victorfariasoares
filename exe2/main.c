a7ab5f8dbb118ddb17a8b0c54eb0779afde70d07
victor <victorfs2@al.insper.edu.br>
2025-03-30 14:13:55 -0300

    lab1 feito novamente

--- exe2/main.c
+++ exe2/main.c
@@ -2,14 +2,35 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-
+const int BTN_BLUE = 7;
+const int BTN_GREEN = 26;
 
 int main() {
   stdio_init_all();
 
+  gpio_init(BTN_BLUE);
+  gpio_init(BTN_GREEN);
+
+  gpio_set_dir(BTN_BLUE, GPIO_IN );
+  gpio_set_dir(BTN_GREEN, GPIO_IN );
+
+  gpio_pull_up(BTN_BLUE);
+  gpio_pull_up(BTN_GREEN);
 
   while (true) {
-    
+    if (!gpio_get(BTN_BLUE)){
+      printf("Botao 1 \n");
+      while(!gpio_get(BTN_BLUE)){
+
+      }
+
+    }
+    if (!gpio_get(BTN_GREEN)){
+      printf("Botao 2 \n");
+      while(!gpio_get(BTN_GREEN)){
+        
+      }
+    }
   }
 }
 
