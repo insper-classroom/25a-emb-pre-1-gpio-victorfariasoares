56c0d83508a06bc30cd62e6ea80f5ca2f176472b
victor <victorfs2@al.insper.edu.br>
2025-03-30 14:19:07 -0300

    lab1 feito novamente 2.0

--- exe2/main.c
+++ exe2/main.c
@@ -2,54 +2,37 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-const int BTN_BLUE = 7;
-const int BTN_GREEN = 26;
+const int BTN_AZUL = 7;
+const int BTN_VERDE = 26;
 
 int main() {
   stdio_init_all();
 
-  gpio_init(BTN_BLUE);
-  gpio_init(BTN_GREEN);
+  gpio_init(BTN_AZUL);
+  gpio_init(BTN_VERDE);
 
-  gpio_set_dir(BTN_BLUE, GPIO_IN );
-  gpio_set_dir(BTN_GREEN, GPIO_IN );
+  gpio_set_dir(BTN_AZUL, GPIO_IN );
+  gpio_set_dir(BTN_VERDE, GPIO_IN );
 
-  gpio_pull_up(BTN_BLUE);
-  gpio_pull_up(BTN_GREEN);
+  gpio_pull_up(BTN_AZUL);
+  gpio_pull_up(BTN_VERDE);
 
   while (true) {
-    if (!gpio_get(BTN_BLUE)){
+    if (!gpio_get(BTN_AZUL)){
       printf("Botao 1 \n");
-      while(!gpio_get(BTN_BLUE)){
+      while(!gpio_get(BTN_AZUL)){
 
       }
 
     }
-    if (!gpio_get(BTN_GREEN)){
+    if (!gpio_get(BTN_VERDE)){
       printf("Botao 2 \n");
-      while(!gpio_get(BTN_GREEN)){
+      while(!gpio_get(BTN_VERDE)){
         
       }
     }
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
