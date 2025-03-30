d7da6e3597c226be7953def9ae52c4caa3beada9
victor <victorfs2@al.insper.edu.br>
2025-03-30 13:34:42 -0300

    tentando ver o que deu de erro

--- exe2/main.c
+++ exe2/main.c
@@ -2,12 +2,31 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-
+const int BTN_VERDE = 26;
+const int BTN_AZUL = 7;
 
 int main() {
-  
+  stdio_init_all();
+
+  gpio_init(BTN_VERDE);
+  gpio_init(BTN_AZUL);
+
+  gpio_set_dir(BTN_VERDE, GPIO_IN);
+  gpio_set_dir(BTN_AZUL, GPIO_IN);
+
+  gpio_pull_up(BTN_VERDE);
+  gpio_pull_up(BTN_AZUL);
 
   while (true) {
-  
+    if (!gpio_get(BTN_VERDE)) {
+      printf("Botao 1\n");
+      while (!gpio_get(BTN_VERDE)) {
+      };
+    }
+    if (!gpio_get(BTN_AZUL)){
+      printf("Botao 2\n");
+      while (!gpio_get(BTN_AZUL)) {
+      };
+    }
   }
 }
